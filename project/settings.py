f5c554b627df9efe336e4f1af7d9464ba3af2068
nabil alaa <nabilalaa06@gmail.com>
2025-07-18 23:10:52 +0300

    ecommerce

--- project/settings.py
+++ project/settings.py
@@ -102,8 +102,12 @@ WSGI_APPLICATION = "project.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "neondb",
+        "USER": "neondb_owner",
+        "PASSWORD": "npg_7DnpH4uolGks",
+        "HOST": "ep-proud-cake-adflxd0a-pooler.c-2.us-east-1.aws.neon.tech",
+        "PORT": 5432,
     }
 }
 # Password validation
