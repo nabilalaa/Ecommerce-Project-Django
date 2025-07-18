25c3b7828d32a29b121f9b1a2417318efa98501e
nabil alaa <nabilalaa06@gmail.com>
2025-07-18 22:13:39 +0300

    ecommerce

--- project/settings.py
+++ project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "rtd4c33bt*af70vb0w1k(^=zv^t4@9nl0xt166!==a9vn9bi=$"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [".vercel.app", "127.0.0.1"]
 
